462f3f5b6129c532752ebfe0308d1a24aaa423fb
TechyTag05 <techy05tag@gmail.com>
2025-05-12 08:33:44 +0530

    icons updated and new sections added

--- js/custom.js
+++ js/custom.js
@@ -66,7 +66,7 @@ $(document).ready(function () {
   // Animate the scroll to top
   $(".go-top").click(function (event) {
     event.preventDefault();
-    $("html, body").animate({ scrollTop: 0 }, 5000);
+    $("html, body").animate({ scrollTop: 0 }, 500);
   });
 
   /* wow
